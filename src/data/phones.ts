93623bda933573f5106ebdc9e73ba70d4d3c4e67
Igor Minayeu <igor.minayeu@gmail.com>
2026-04-23 17:50:03 +0300

    add data

---
+++ src/data/phones.ts
@@ -0,0 +1,59 @@
+export type PhoneType = {
+    id: string
+    category: 'phones'
+    image: string
+    name: string
+    price: number
+}
+
+export const goods: PhoneType[] = [
+    {
+        id: crypto.randomUUID(),
+        category: 'phones',
+        image: "https://imgproxy.onliner.by/gQfxPdToPX8tNAYx0wzfvDxhZ5HXCcVllTzr7mRmHDE/w:170/h:250/z:2/f:jpg/aHR0cHM6Ly9jb250/ZW50Lm9ubGluZXIu/YnkvY2F0YWxvZy9k/ZXZpY2Uvb3JpZ2lu/YWwvNmViMmViMmJi/OGFmMWUyYjQ3ZmYy/YjYwNGI4ZWQ2NWUu/anBn",
+        name: 'POCO X8 Pro Max',
+        price: 1749
+    },
+    {
+        id: crypto.randomUUID(),
+        category: 'phones',
+        image: "https://imgproxy.onliner.by/MC3tLz3lf-VhbOIDOd-zrgKSJiXi1m6deDAJB-L0Mec/w:170/h:250/z:2/f:jpg/aHR0cHM6Ly9jb250/ZW50Lm9ubGluZXIu/YnkvY2F0YWxvZy9k/ZXZpY2Uvb3JpZ2lu/YWwvZDc5M2U5ODUz/YTQ0MjEyZDE0ZjJl/MWUwNmJlMzNiNWQu/anBn",
+        name: 'HONOR X9d',
+        price: 1020
+    },
+    {
+        id: crypto.randomUUID(),
+        category: 'phones',
+        image: "https://imgproxy.onliner.by/yw1WygDUYds-eJJbmahaoFSt28owBM-kcu2Hq-EW2E0/w:170/h:250/z:2/f:jpg/aHR0cHM6Ly9jb250/ZW50Lm9ubGluZXIu/YnkvY2F0YWxvZy9k/ZXZpY2Uvb3JpZ2lu/YWwvNGQxNDg5ZjVk/ZjM3NDhhNmE3MDc2/ZTA5MmMzNTI5Yzku/anBn",
+        name: 'Apple iPhone 17 Pro',
+        price: 4050
+    },
+    {
+        id: crypto.randomUUID(),
+        category: 'phones',
+        image: "https://imgproxy.onliner.by/qVHNPAfgFY4lmqT6ym8B8ufr4lyIgFVLOa6aDBdbyNQ/w:170/h:250/z:2/f:jpg/aHR0cHM6Ly9jb250/ZW50Lm9ubGluZXIu/YnkvY2F0YWxvZy9k/ZXZpY2Uvb3JpZ2lu/YWwvNzE3ODZlMWZl/MjMxNzRkOTA4MmI5/ZDJhZWZlZDgyMzUu/anBn",
+        name: 'Samsung Galaxy S26 Ultra',
+        price: 3669
+    },
+    {
+        id: crypto.randomUUID(),
+        category: 'phones',
+        image: "https://imgproxy.onliner.by/uVvdj9hMgmg24e89wLTxU7ppdrwL3Q2UAB5fKE41vbc/w:170/h:250/z:2/f:jpg/aHR0cHM6Ly9jb250/ZW50Lm9ubGluZXIu/YnkvY2F0YWxvZy9k/ZXZpY2Uvb3JpZ2lu/YWwvMzQ5YjJlZGQw/NjVhNTczMzViYjY3/YzQ2YTc2NDA2MTUu/anBn",
+        name: 'Apple iPhone 16 Pro',
+        price: 3401
+    },
+    {
+        id: crypto.randomUUID(),
+        category: 'phones',
+        image: "https://imgproxy.onliner.by/q3B1-kon-8bJy10XIvbV0WBXm1TJU21pbD_hcKheO4o/w:170/h:250/z:2/f:jpg/aHR0cHM6Ly9jb250/ZW50Lm9ubGluZXIu/YnkvY2F0YWxvZy9k/ZXZpY2Uvb3JpZ2lu/YWwvZTFiZjQ0MTk4/NjBlNmYwNDIwMjFh/NGFlMjdkMmM5M2Yu/cG5n",
+        name: 'Google Pixel 10a',
+        price: 1690
+    },
+    {
+        id: crypto.randomUUID(),
+        category: 'phones',
+        image: "https://imgproxy.onliner.by/QV2lVyl9UKdb2LkTWRxwTpSYJVV297z8A5ZbEl_yngM/w:170/h:250/z:2/f:jpg/aHR0cHM6Ly9jb250/ZW50Lm9ubGluZXIu/YnkvY2F0YWxvZy9k/ZXZpY2Uvb3JpZ2lu/YWwvMjk2NzljMjY5/MjdjZGI0NDFjODMw/MDBiMmUwMjgwMmMu/anBn",
+        name: 'HONOR 600 Lite',
+        price: 924
+    }
+]
